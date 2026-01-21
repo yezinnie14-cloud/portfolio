778b1418da8d89d5ce58d70d00cb48f600807cac
yezinnie14 <yezinnie14@gmail.com>
2026-01-21 17:54:57 +0900

    0121 업데이트 마지막이 아니었다는

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,13 @@
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className="contact">
+    <div className="send-box">
+      <p>Send a Message</p>
+      
+    </div>
+    <div className="connect-box">
+      </div> 
+      </div>
   )
 }
 
